a929fa549eefba762390a1b6af562a4ea684aada
Zubair <zubairq@gmail.com>
2019-07-23 13:37:09 +0100

    Can load a Vue app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<div style="font-size:50px;font-family: helvetica; font-weight:bold;">Learno</div>')
+  res.send(fs.readFileSync("public/index.html").toString())
 });
 
 app.listen(80, () => {
