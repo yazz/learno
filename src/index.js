9eaca7161f5a3f831d6153d09ead7f05ea20c89e
Zubair <zubairq@gmail.com>
2019-07-23 10:07:38 +0100

    Make Learno run on port 80

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+app.listen(80, () => {
+  console.log('Example app listening on port 80!')
 });
