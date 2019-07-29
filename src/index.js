5c029caf94dc71a7e97d05fa0a99c219bdaacc05
Zubair <zubairq@gmail.com>
2019-07-29 18:40:42 +0100

    Can connect to Postgress database now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express       = require('express')
 const fs            = require('fs')
 const compression   = require('compression')
+const postgresdb    = require('pg')
 
 const app = express();
 app.use(compression())
@@ -11,4 +12,36 @@ app.get('/', (req, res) => {
 
 app.listen(80, () => {
   console.log('Example app listening on port 80!')
+});
+
+app.get('/learno', (req, res) => {
+    var config = {
+      user:              "postgres",
+      database:          "postgres",
+      password:          "postgres",
+      host:              "127.0.0.1",
+      port:              5432
+    };
+    var dbconnection = new postgresdb.Client(config);
+    dbconnection.connect(function (err) {
+      if (err) {
+          console.log({error: '' + err});
+      } else {
+              useSql = "SELECT tablename as name FROM pg_catalog.pg_tables where schemaname = 'public';"
+          dbconnection.query(useSql, [], function (err, result) {
+            if (err) {
+                console.log({failed: '' + err});
+            } else {
+                console.log("row count: " + result.rows.length); // outputs: { name: 'brianc' }
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(
+                    result.rows
+                ));
+            };
+          })
+
+      }
+    });
+
+
 });
