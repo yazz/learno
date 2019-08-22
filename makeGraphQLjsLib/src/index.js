ce41c925ce7ca01d1195039034d56871e200f3ed
Zubair <zubairq@gmail.com>
2019-08-22 23:26:25 +0100

    Trying to get graphQL working

--- makeGraphQLjsLib/src/index.js
+++ makeGraphQLjsLib/src/index.js
@@ -2,6 +2,13 @@ import gql from 'graphql-tag';
 
 import { OfflineClient } from '@aerogear/voyager-client';
 
+import { ApolloClient, createNetworkInterface } from 'apollo-client'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
+
+var networkInterface = createNetworkInterface({
+  uri: 'http://127.0.0.1:80/graphql',
+})
+
 import {
   createClient,
   strategies
@@ -14,3 +21,35 @@ window.gql = gql
 
 window.createClient = createClient
 window.strategies = strategies
+
+const wsClient = new SubscriptionClient('ws://localhost:3020/subscriptions', {
+  reconnect: true,
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient,
+)
+
+
+const GET_DOG = gql`
+query {
+  getTest(id: 341) {
+    id
+    name
+    questions {
+      id
+      question
+    }
+  }
+}
+`;
+console.log(GET_DOG);
+
+const client = new ApolloClient({
+  cacheRedirects: {
+    Query: {
+      dog: (_, { id }, { getCacheKey }) => getCacheKey({ id, __typename: 'Dog' })
+    }
+  }
+})
