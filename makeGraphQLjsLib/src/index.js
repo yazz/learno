a46d1a52bad22c24c42a6f0ef439add63fe8c624
Zubair <zubairq@gmail.com>
2019-08-23 21:55:02 +0100

    ipad1

--- makeGraphQLjsLib/src/index.js
+++ makeGraphQLjsLib/src/index.js
@@ -7,8 +7,8 @@
 //window.OfflineClient = OfflineClient
 //window.gql = gql
 alert("oo")
-//import { createHttpLink } from "apollo-link-http";
-//alert(2)
+import { createHttpLink } from "apollo-link-http";
+alert(2)
 //import { InMemoryCache } from "apollo-cache-inmemory";
 //alert(3)
 
