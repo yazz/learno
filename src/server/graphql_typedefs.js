3b5d00874b40dcbf4626ec4e6ec35fb272138418
Zubair <zubairq@gmail.com>
2019-09-03 07:11:42 +0200

    reformat code

--- src/server/graphql_typedefs.js
+++ src/server/graphql_typedefs.js
@@ -10,12 +10,14 @@ const { gql }    = require('@aerogear/voyager-server')
 
 
 module.exports = gql`
+
   type Question {
       id:Int
       question: String
       category: String
 
   }
+
 
 
   type Test {
@@ -28,17 +30,23 @@ module.exports = gql`
   }
 
 
+
   type User {
       id: Int
       user_name: String
   }
 
 
+
   type Query {
-    getQuestions: [Question]
-    getTests: [Test]
-    getTest(id: Int): Test
-    getUsers: [User]
-    getTopCourses: [Test]
+    getQuestions:       [Question]
+
+    getTests:           [Test]
+
+    getTest(id: Int):    Test
+
+    getUsers:           [User]
+    
+    getTopCourses:      [Test]
   }
 `
